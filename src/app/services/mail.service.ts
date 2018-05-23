18b13567eeaf4846444892b440237cea0c619e70
Gerkin <nihilivin@gmail.com>
2018-05-23 18:36:31 +0200

    Updated Diaspora version to 0.3.0 in MailService

--- src/app/services/mail.service.ts
+++ src/app/services/mail.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-const Diaspora = require( 'diaspora/dist/standalone/diaspora.min' );
+import { Diaspora } from '@diaspora/diaspora';
 
 import { environment} from '../../environments/environment';
 import { IIthoughtsEnvironment } from '../../environments/environment.common';
